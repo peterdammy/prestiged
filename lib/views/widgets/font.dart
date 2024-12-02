7790ebec32d4297cd0a2db20de6b8bbc5f9b7993
peterdammy <peterosunsan2@gmail.com>
2024-12-02 19:43:38 +0100

    second commit

---
+++ lib/views/widgets/font.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppText {
+  static const headingText = TextStyle(
+    fontSize: 24,
+    fontFamily: 'Roobert',
+    fontWeight: FontWeight.w900,
+  );
+  static const bodyText = TextStyle(
+    fontSize: 14,
+    fontFamily: 'Roobert',
+    fontWeight: FontWeight.w400,
+  );
+  static const actionText = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Roobert',
+    fontWeight: FontWeight.w700,
+  );
+}
